40fb00886789124c035ba9ada754673661e85ede
zhouxueliang <525198962@qq.com>
2017-03-07 14:38:49 +0800

    Initial Commit

---
+++ SchoolCollection/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SchoolCollection
+//
+//  Created by 紫薇大舅 on 2017/3/7.
+//  Copyright © 2017年 ziweidajiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
